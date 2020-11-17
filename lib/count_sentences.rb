5b2b00a4415b9f014cf6871b141980bbbd03c0a5
Tyler Young <tyoung900@gmail.com>
2020-11-17 22:15:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.endwith?(".")
   end
 
   def question?
-
+    self.endwith?("?")
   end
 
   def exclamation?
-
+    self.endwith?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
